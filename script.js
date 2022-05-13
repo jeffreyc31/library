47b317cb30c69d0ce6e67f87d165d05fd248374e
jeffreyc31 <jeffcgit31@gmail.com>
2022-05-13 14:08:11 -0700

    create myLibrary array, Book object constructor, addBook function
    - addBook creates a new card with user input on HTML

---
+++ script.js
@@ -0,0 +1,31 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+const potter = new Book("harry potter", "jk rowling", '1');
+myLibrary.push(potter);
+
+
+function addBook() {
+    myLibrary.forEach(book => {
+        let cardGrid = document.getElementById('card-grid');
+        let newCard = document.createElement('div');
+        let bookTitle = document.createElement('h3');
+        let bookAuthor = document.createElement('h3');
+        let bookPages = document.createElement('h3');
+
+        newCard.setAttribute('class', 'card');
+        bookTitle.innerText = book.title;
+        bookAuthor.innerText = book.author;
+        bookPages.innerText = book.pages;
+
+        cardGrid.appendChild(newCard);
+        newCard.appendChild(bookTitle);
+        newCard.appendChild(bookAuthor);
+        newCard.appendChild(bookPages);
+    });
+}
